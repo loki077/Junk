856fa2b5c25e8a32b96386aa39d4054691737bf0
Lokesh <loki077@gmail.com>
2023-07-05 14:25:04 +1000

    Flask Example

---
+++ Python/flask/demo-app/main.py
@@ -0,0 +1,14 @@
+# main.py
+
+from flask import Flask
+
+
+app = Flask(__name__)
+
+@app.route("/")
+
+def index():
+    return "Configuration, it's a web page"
+
+if __name__ == "__main__":
+    app.run(host="127.0.0.1", port=8080, debug=True)
